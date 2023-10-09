d9198480a124231985b33781a70da31b9d685a7f
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-10-09 15:36:39 +0200

    update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:02:52 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/09 15:13:11 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/09 15:33:09 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 char	*get_next_line(int fd);
 
