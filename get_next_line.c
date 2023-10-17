8091eb3e7bd13f2738f52ee47f09000c12e9bc48
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-10-17 16:46:56 +0200

    update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,42 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 10:51:16 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/17 16:13:53 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/17 16:39:52 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_create_line(char *str)
+char    *ft_create_line(char *str)
 {
-	int		i;
-	char	*result;
+    int i;
+    static char *result;
 
-	i = 0;
-	while (str[i] != '\n' && str[i])
-		i++;
-	if (!str[i])
-		result = (char *)malloc(i + 1);
-	else
-	{
-		result = (char *)malloc(i + 2);
-		result[i + 1] = 0;
-	}
-	while (i >= 0)
-	{
-		result[i] = str[i];
-		i--;
-	}
-	return (result);
+    i = 0;
+    while (str[i] != '\n' && str[i])
+        i++;
+    if (!str[i])
+        result = (char *)malloc(i + 1);
+    else
+    {
+        result = (char *)malloc(i + 2);
+        result[i + 1] = 0;
+    }
+    while (i >= 0)
+    {
+        result[i] = str[i];
+        i--;
+    }
+    return (result);
 }
 
 char	*ft_delete_garbage(char *str)
 {
 	int		i;
 	int		size;
-	char	result[BUFFER_SIZE];
+	char	*result;
 
+	result = malloc(BUFFER_SIZE);
 	size = 0;
 	while (str[size] != '\n')
 		size++;
@@ -55,15 +56,19 @@ char	*ft_delete_garbage(char *str)
 		result[i++] = str[size];
 		size++;
 	}
+	free(str);
 	return (result);
 }
 
 char	*get_next_line(int fd)
 {
 	int			data;
-	static char	buffer[BUFFER_SIZE];
+	static char	*buffer;
 	char		*temporal;
 	char		*result;
+	
+	//------ASIGNAR MEMORIA AL BUFFER------//
+	buffer = malloc(BUFFER_SIZE);
 
 	//------LEER EL ARCHIVO------//
 	while (!ft_strchr(temporal, '\n'))
