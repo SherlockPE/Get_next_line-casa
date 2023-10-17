7728b0f03a7cbf320b919130d1ed6f873e7deeb4
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-10-17 17:44:58 +0200

    update get_next_line.c and main.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,33 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 10:51:16 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/17 16:39:52 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:33:15 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *ft_create_line(char *str)
+char	*ft_create_line(char *str)
 {
-    int i;
-    static char *result;
+	int		i;
+	char	*result;
 
-    i = 0;
-    while (str[i] != '\n' && str[i])
-        i++;
-    if (!str[i])
-        result = (char *)malloc(i + 1);
-    else
-    {
-        result = (char *)malloc(i + 2);
-        result[i + 1] = 0;
-    }
-    while (i >= 0)
-    {
-        result[i] = str[i];
-        i--;
-    }
-    return (result);
+	i = 0;
+	while (str[i] != '\n' && str[i])
+		i++;
+	if (!str[i])
+		result = (char *)malloc(i + 1);
+	else
+	{
+		result = (char *)malloc(i + 2);
+		result[i + 1] = 0;
+	}
+	while (i >= 0)
+	{
+		result[i] = str[i];
+		i--;
+	}
+	return (result);
 }
 
 char	*ft_delete_garbage(char *str)
@@ -47,39 +47,38 @@ char	*ft_delete_garbage(char *str)
 		size++;
 	size++;
 	i = 0;
-	if (str[size] == 0)
+	if (!str[size])
 	{
-		result[0] = 0;
+		free(str);
+		return (0);
 	}
 	while (str[size])
-	{
-		result[i++] = str[size];
-		size++;
-	}
+		result[i++] = str[size++];
 	free(str);
 	return (result);
 }
 
 char	*get_next_line(int fd)
 {
-	int			data;
-	static char	*buffer;
+	ssize_t		data;
+	char		*buffer;
+	static char	*result;
 	char		*temporal;
-	char		*result;
-	
-	//------ASIGNAR MEMORIA AL BUFFER------//
-	buffer = malloc(BUFFER_SIZE);
 
+	data = 7;
+	//------ASIGNAR MEMORIA AL BUFFER------//
+	buffer = malloc(BUFFER_SIZE + 1);
 	//------LEER EL ARCHIVO------//
-	while (!ft_strchr(temporal, '\n'))
+	while (!ft_strchr(result, '\n') && data)
 	{
 		data = read(fd, buffer, BUFFER_SIZE);
 		if (data == -1)
 			return (0);
-		temporal = ft_strjoin(temporal, buffer);
+		result = ft_strjoin(result, buffer);
 	}
 	//------CREAR SUBSTRING------//
-	result = ft_create_line(temporal);
-	temporal = ft_delete_garbage(temporal);
-	return (result);
+	temporal = ft_create_line(result);
+	result = ft_delete_garbage(result);
+	free(buffer);
+	return (temporal);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:02:20 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/10/16 17:47:07 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:29:35 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	main(int argc, char *argv[])
 {
 	(void)argc;
-	
 	int i;
 	char * line;
 	//system("leaks a.out");
@@ -29,13 +28,13 @@ int	main(int argc, char *argv[])
 	}
 	
 	//------Imprimir las lineas------//
-	i = 3;
+	i = 4;
 	while (i--)
 	{
 		printf("Llamando a la funcion get_next_line...)\n");
 		line = get_next_line(fd);
-		printf("%s\n", line);
+		printf("%s", line);
+		free(line);
 	}
-	free(line);
 	return (0);
 }
